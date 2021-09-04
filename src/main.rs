41e9aae01b4dec0f0d29a71bd5842e5a7b77bc26
H. Lee <lhslhg@gmail.com>
2021-09-04 15:37:03 +0900

    feat: construct initial hello_triangle_application struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate glfw;
+
+struct hello_triangle_application {
+
+}
+
+impl hello_triangle_application {
+    pub fn run(&mut self) {
+        self.init_vulkan();
+        self.main_loop();
+    }
+
+    fn init_vulkan(&mut self){
+    }
+
+    fn main_loop(&mut self){
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
 }
